0702c69d811d313b60ab5298a38bec92bd77876c
sirius1y <sirius1y@outlook.com>
2023-10-12 19:15:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CourseSystem
---
+++ src/test/java/com/database/coursesystem/CourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.coursesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
