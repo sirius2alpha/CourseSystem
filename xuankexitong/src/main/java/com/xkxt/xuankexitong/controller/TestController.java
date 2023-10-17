88e74f35648ad226d32f1d648d23f58bcb2d40fe
sirius1y <sirius1y@outlook.com>
2023-10-17 09:24:35 +0800

    在main分支上，已添加xuankexitong索引

---
+++ xuankexitong/src/main/java/com/xkxt/xuankexitong/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.xkxt.xuankexitong.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "ok";
+    }
+}
