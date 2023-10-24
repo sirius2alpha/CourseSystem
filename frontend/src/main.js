9c5a82e18571d446a520d97acad4e036caed083c
sirius1y <sirius1y@outlook.com>
2023-10-24 22:41:14 +0800

    add element-plus packages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 // 引入路由
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+
+createApp(App).use(router).use(ElementPlus).mount('#app');
 
