44a9d4e79b398052d152617de24f6b8dbf06ef41
chril <xieyiwen.1008@gmail.com>
2023-10-26 16:13:46 +0800

    选课完善

--- CourseSystem-main/project_springboot/src/main/java/com/example/project/controller/SelectcourseController.java
+++ CourseSystem-main/project_springboot/src/main/java/com/example/project/controller/SelectcourseController.java
@@ -4,6 +4,7 @@ package com.example.project.controller;
 import com.example.project.common.Result;
 import com.example.project.entity.*;
 import com.example.project.service.*;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,7 +42,7 @@ public class SelectcourseController {
     }
 
     @GetMapping ("/courses")
-    public Result msg(@RequestBody Selectcourse selectcourse){
+    public Result selclass(@RequestBody Selectcourse selectcourse){
         Integer courseid=selectcourse.getCourse_id();
         String coursename=selectcourse.getCourse_name();
         Integer teacherid=selectcourse.getTeacher_id();
@@ -129,7 +130,7 @@ public class SelectcourseController {
             List<Teachers> teachersname=teachersService.lambdaQuery()
                     .eq(Teachers::getId,teacherid).list();
             res.put("teacher_name",teachersname.get(0).getName());
-            res.put("capacity",0);
+            res.put("capacity",50);
             no=list.get(i).getNo();
             List<SelectedCourses> selectno=selectedCoursesService.lambdaQuery()
                     .eq(SelectedCourses::getCurrentCourse_id,no).list();
@@ -139,5 +140,42 @@ public class SelectcourseController {
         }
 
         return list.size()>0?Result.suc(response):Result.fail();
+    }
+
+    @GetMapping("/students/{userId}/courses")
+    public Result seletedclass(@PathVariable("userId") String userId)
+    {
+        Integer userid = Integer.valueOf(userId);
+        System.out.println(userid);
+        List<SelectedCourses> selectno=selectedCoursesService.lambdaQuery()
+                .eq(SelectedCourses::getStudent_id,userid).list();
+        if(selectno.size()==0)
+            return Result.fail();
+        List<LinkedHashMap> response=new ArrayList<>();
+        int courseno,courseid,teacherid;
+        for(int i=0;i<selectno.size();i++)
+        {
+            LinkedHashMap res=new LinkedHashMap();
+            courseno=selectno.get(i).getCurrentCourse_id();
+            List<CurrentCourses> list = currentCoursesService.lambdaQuery()
+                    .eq(CurrentCourses::getNo,courseno).list();
+            courseid=list.get(0).getCourseId();
+            res.put("course_id",courseid);
+            List<CoursePlan> coursesname=coursePlanService.lambdaQuery()
+                    .eq(CoursePlan::getCourseId,courseid).list();
+            res.put("course_name",coursesname.get(0).getCourseName());
+            teacherid=list.get(0).getTeacherId();
+            res.put("teacher_id",teacherid);
+            List<Teachers> teachersname=teachersService.lambdaQuery()
+                    .eq(Teachers::getId,teacherid).list();
+            res.put("teacher_name",teachersname.get(0).getName());
+            res.put("capacity",50);
+            List<SelectedCourses> selectno1=selectedCoursesService.lambdaQuery()
+                    .eq(SelectedCourses::getCurrentCourse_id,courseno).list();
+            res.put("selected_number",selectno1.size());
+            res.put("course_time",list.get(0).getTime());
+            response.add(i,res);
+        }
+        return Result.suc(response);
     }
 }
